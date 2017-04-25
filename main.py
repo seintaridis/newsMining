dbbe343f34a40f7b9a49b4bd19098f418c6b972b
seintaridis <jimseinta@hotmail.com>
2017-04-25 16:04:33 +0200

    add stopwords and create wordcloud per category

--- main.py
+++ main.py
@@ -3,28 +3,21 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 df = pd.read_csv('dataSets/train_set.csv', sep='\t')
 
+my_additional_stop_words=['example1','example2']  #our custom stop words
+stop_words = ENGLISH_STOP_WORDS.union(my_additional_stop_words)
 
-f = open('out.txt', 'w')
-print >> f, 'Filename:', df[["Content"]]  # or f.write('...\n')
-f.close()
-
-
-print df[["Content"]]
-
-#print df[["Category"]].iloc[5:10]
-
-A = np.array(df[["Content"]])
-text = ""
-for i in range(A.shape[0]):
-    for j in range(A.shape[1]):
-        text += str(A[i,j]) + ","
-
-#print text
-#Generate a word cloud image
-wordcloud = WordCloud().generate(text)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+#create image for every category
+for category in df.Category.unique():
+    print category
+    text = df.loc[df['Category'] == category]
+    textString=text.Content.to_string()
+    textString = ' '.join([word for word in textString.split() if word not in stop_words])
+    wordcloud = WordCloud().generate(textString)
+    print wordcloud
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
