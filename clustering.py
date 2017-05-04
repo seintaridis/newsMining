eff93b40a561ebd80355b45881e4a34f6c5b5398
leedah <ledacle@ubuntu.(none)>
2017-05-05 02:17:06 +0300

    clustering: nltk, cosine similarity and csv output

--- clustering.py
+++ clustering.py
@@ -1,41 +1,52 @@
+from __future__ import division
 import pandas as pd
+import nltk
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import TSNE
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
-from sklearn.cluster import KMeans
+from nltk.cluster.kmeans import KMeansClusterer
+from nltk.cluster.util import cosine_distance
+import numpy as nmp
 import matplotlib.pyplot as plt
 from matplotlib import style
 style.use("ggplot")
 
 
+# Preproccessing
+
+# read trainset
 
 df = pd.read_csv('dataSets/train_set.csv', sep='\t')
 
-my_additional_stop_words=['Antonia','Nikos','Nikolas']
+# remove stop words
+
+my_additional_stop_words=['said','th','month','much','thing','say','says'] 
 stop_words = ENGLISH_STOP_WORDS.union(my_additional_stop_words)
+
+# create counter and idf vectors
+
 count_vect = TfidfVectorizer    (stop_words=stop_words)
-count_vect.fit(df['Content'].head(12266))
-X_train_counts = count_vect.transform(df['Content'].head(12266))
+count_vect.fit(df['Content'].head(1000)) #12266
+X_train_counts = count_vect.transform(df['Content'].head(1000))
+
+# reduce size of vector with LSI
 
 svd = TruncatedSVD(n_components=5)
 X_train_counts = svd.fit_transform(X_train_counts)
 
-print X_train_counts.shape           #(12266, 85437) the result means
 
-print df['Category'].iloc[5]
+# Clustering 
 
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X_train_counts)
-
-
-centroids = kmeans.cluster_centers_
-labels = kmeans.labels_
-
-countPolitics=0
+kclusterer = KMeansClusterer(num_means = 5, distance=cosine_distance, repeats=25, avoid_empty_clusters= True)
+clusters = kclusterer.cluster(X_train_counts, assign_clusters=True)
+# print "Clusters:\n " , clusters
+# print "Means" , kclusterer.means()
 
 
-categorys_map={
+# Prepare results Matrix
+
+categories_map={
 'Politics': 0,
 'Business': 1,
 'Film': 2,
@@ -43,22 +54,35 @@ categorys_map={
 'Football': 4
 }
 
-#print labels
+labels_map={
+'Cluster1': 0,
+'Cluster2': 1,
+'Cluster3': 2,
+'Cluster4': 3,
+'Cluster5': 4
+}
 
-w, h = 5, 5;
-Matrix = [[0 for x in range(w)] for y in range(h)]
+w, h =  len(labels_map), len(categories_map);
 
+Matrix = nmp.array ([[0 for x in range(w)] for y in range(h)], dtype=float)
+rowsum = [0 for x in range(w)]
 
-print categorys_map['Politics']
+# labels = kclusterer.cluster_names()
+# means = kclusterer.means()
 
+for index in range(len(clusters)): # 0 to 19
+    Matrix[clusters[index]][categories_map[df['Category'].iloc[index]]] += 1
+print "\nWords in clusters per category:\n", Matrix
 
+rowsum = nmp.sum(Matrix, axis=1)
+# print "rowsum:\n", rowsum
 
-#print Matrix
-for index in range(len(labels)):
-    Matrix[labels[index]][categorys_map[df['Category'].iloc[index]]] += 1
-print Matrix
+for i in range(w):	
+	for j in range(h):
+		x  = Matrix[i][j] / rowsum[i]
+		Matrix[i][j] = float("{0:.2f}".format(x))
 
-#print(centroids)
+print "\nPercentage of words in clusters per category\n", Matrix
 
 colors = ["g.","r.","c.","y.","m."]
 
@@ -68,12 +92,14 @@ X_train_counts = svd.fit_transform(X_train_counts)
 #reduced = TSNE(perplexity=30.0).fit_transform(X_train_counts)
 
 for i in range(X_train_counts.shape[0]):
-    plt.plot(X_train_counts[i, 0], X_train_counts[i, 1], colors[labels[i]], markersize = 10)
+    plt.plot(X_train_counts[i, 0], X_train_counts[i, 1],colors[clusters[i]], markersize = 10)
 
+# plt.scatter(centroids[:, 0],centroids[:, 1], marker = "x", s=150, linewidths = 5, zorder = 10)
 
-plt.scatter(centroids[:, 0],centroids[:, 1], marker = "x", s=150, linewidths = 5, zorder = 10)
+labels_sorted = sorted(labels_map.keys())
 
+df_cluster = pd.DataFrame(Matrix, index = labels_sorted, columns = categories_map)
+
+df_cluster.to_csv('clustering_KMeans.csv', sep='\t')
 plt.show()
 
-
-#print(labels)
