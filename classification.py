e7da06dd558bda5f8e531565468435089af739a5
seintaridis <jimseinta@hotmail.com>
2017-05-06 18:04:12 +0200

    create csv for classification

--- classification.py
+++ classification.py
@@ -13,38 +13,68 @@ from sklearn import svm
 from sklearn.model_selection import KFold
 import numpy as np
 from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from csvReader import writeStats
+from sklearn.metrics.pairwise import euclidean_distances
+from collections import Counter
+from sklearn.metrics import recall_score
+
+classification_method_array=['naiveBayes','RandomForest','SVM','KNN']
+
+def knn(test_index,X_train_counts,X_test_counts,train_index):
+    yPred = []
+    for test, i in enumerate(test_index):
+        # create list for distances and targets
+        distances = euclidean_distances(X_train_counts, [X_test_counts[test]])
+        distances = zip(distances, df["Category"].iloc[train_index])
+
+        # sort the list
+        distances.sort()
+
+        # print "our test is " + df["Category"].iloc[i]
+        # make a list of the k neighbors' targets
+        targets = [distances[x][1] for x in range(5)]
+
+        # print targets
+        c = Counter(targets)
+        # print c.most_common(1)
+        #    print targets
+        yPred.append(c.most_common(1)[0][0])
+    return yPred
+
+
+
+
+def classificationMethod(method,X_train_counts,X_test_counts,category_index,train_index,test_index):
+    yPred=None;
+    if method == 'naiveBayes':
+        clf_cv = GaussianNB().fit(X_train_counts,category_index)
+    elif method == 'RandomForest':
+        clf_cv = RandomForestClassifier(n_estimators=5).fit(X_train_counts,df['Category'].iloc[train_index])
+    elif method == 'SVM':
+        clf_cv = svm.SVC().fit(X_train_counts,category_index)
+    elif method == 'KNN':
+        return knn(test_index,X_train_counts,X_test_counts,train_index)
+    yPred = clf_cv.predict(X_test_counts)
+    return yPred;
+
 
 style.use("ggplot")
 df = pd.read_csv('dataSets/train_set.csv', sep='\t')
+df =df.head(1000)
 
-my_additional_stop_words=['Antonia','Nikos','Nikolas']
+my_additional_stop_words=['Antonia','Nikos','Nikolas']  #check improvement with stop_words
 stop_words = ENGLISH_STOP_WORDS.union(my_additional_stop_words)
 
 
-#svd = TruncatedSVD(n_components=5)
-#X_train_counts = svd.fit_transform(X_train_counts)
-
-#print X_train_counts.shape           #(12266, 85437) the result means
-
-
-#clf = MultinomialNB().fit(X_train_counts,df.Category.head(10000))  #naive_bayes
-#clf = RandomForestClassifier(n_estimators=5).fit(X_train_counts,df.Category.head(10)) #RandomForestClassifier
-#clf = svm.SVC().fit(X_train_counts,df.Category.head(10000))
-
-
-#docs_new = ['Britain new', 'facebook google']
-#X_new_counts = count_vect.transform(docs_new)
-
-#print X_new_counts
-
-#predicted = clf.predict(X_new_counts)
-
-#print predicted
-#for doc, category in zip(docs_new, predicted):
- # print('%r => %s' % (doc,category))
-
 #cross validation
-kf = KFold(n_splits=10)
+averageAccurracy=0
+averageAccurracyArray=[0,0,0,0]
+averagePrecisionArray=[0,0,0,0]
+averageRecallArray=[0,0,0,0]
+nFolds=10
+kf = KFold(n_splits=nFolds)
 fold = 0
 count_vect = TfidfVectorizer(stop_words=stop_words)
 count_vect.fit(df['Content'])
@@ -58,9 +88,34 @@ for train_index, test_index in kf.split(df):
      X_test_counts = np.add(X_test_counts, count_vect.transform(df['Title'].iloc[test_index]))
      X_train_counts = svd.transform(X_train_counts)
      X_test_counts = svd.transform(X_test_counts)
-     clf_cv = RandomForestClassifier(n_estimators=5).fit(X_train_counts,df['Category'].iloc[train_index])
-     yPred = clf_cv.predict(X_test_counts)
-     print yPred
-     fold += 1
+    # clf_cv = RandomForestClassifier(n_estimators=5).fit(X_train_counts,df['Category'].iloc[train_index])
+    # clf_cv = GaussianNB().fit(X_train_counts,df['Category'].iloc[train_index])
      print "Fold " + str(fold)
-     print(classification_report(yPred,df['Category'].iloc[test_index], target_names=df.Category.unique()))
+     for idx,val in enumerate(classification_method_array):
+         print val
+         yPred = classificationMethod(val,X_train_counts,X_test_counts,df['Category'].iloc[train_index],train_index,test_index)
+         averageAccurracyArray[idx] += accuracy_score(df['Category'].iloc[test_index],yPred)
+         averagePrecisionArray[idx] += precision_score(df['Category'].iloc[test_index], yPred, average='macro')
+         averageRecallArray[idx] += recall_score(df['Category'].iloc[test_index], yPred, average='macro')
+         print "accuracy"
+         print accuracy_score(df['Category'].iloc[test_index],yPred)
+         print "precision"
+         print precision_score(df['Category'].iloc[test_index], yPred, average='micro')
+         print "recall"
+         print recall_score(df['Category'].iloc[test_index], yPred, average='macro')
+     #yPred = clf_cv.predict(X_test_counts)
+    # print yPred
+     fold += 1
+    # print "Fold " + str(fold)
+    # print(classification_report(yPred,df['Category'].iloc[test_index], target_names=df.Category.unique()))
+     #print "accuracy"
+     #print accuracy_score(df['Category'].iloc[test_index],yPred)
+     #averageAccurracy+=accuracy_score(df['Category'].iloc[test_index],yPred)
+     #print "precision"
+     #print precision_score(df['Category'].iloc[test_index], yPred, average='micro')
+averageAccurracy=averageAccurracy/nFolds
+for idx,val in enumerate(averageAccurracyArray):
+    averageAccurracyArray[idx] =averageAccurracyArray[idx]/nFolds
+    averagePrecisionArray[idx] =averagePrecisionArray[idx]/nFolds
+    averageRecallArray[idx]=averageRecallArray[idx]/nFolds
+writeStats(averageAccurracyArray,averagePrecisionArray,averageRecallArray)
